88e9d8cea35e3bcf5b6160a7c527092ada77c7e1
akmur <akmurzinmihail@gmail.com>
2023-12-21 22:10:37 +0500

    Начало работы

---
+++ src/task.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace task1{
+    void task1_main();
+}
+
+namespace task2{
+    void task2_main();
+}
+
+namespace task3{
+    void task3_main();
+}
+
+namespace task4{
+    void task4_main();
+}
